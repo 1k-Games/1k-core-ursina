0eb50d3e84ed670b6d241b0416012c477bea455b
konvay <parisschuette@gmail.com>
2023-07-24 23:04:16 -0500

    Expand inputs/camera controls for third person

--- player_controllers/third_person_controller.py
+++ player_controllers/third_person_controller.py
@@ -9,21 +9,40 @@ class ThirdPersonController(Entity):
         self.model = "cube"
         self.color = color.red
 
+        self.mouse_sensitivity = Vec2(40, 40)
+
         camera.parent = self
         camera.fov = 90
         camera.y = 1.25
         camera.z = -3.25
 
-    def update(self):
-        self.rotation_y += ((held_keys['d'] - held_keys['a']) or held_keys['gamepad left stick x'])
+        mouse.locked = True
 
+    def update(self):
+        direction = ((held_keys['w'] - held_keys['s']) or held_keys['gamepad left stick y'])
+        rotation = ((held_keys['d'] - held_keys['a']) or held_keys['gamepad left stick x'])
+        
+        self.rotation_y += rotation
         self.direction = Vec3(
-            self.forward * ((held_keys['w'] - held_keys['s']) or held_keys['gamepad left stick y'])
+            self.forward * direction
             ).normalized()
         move_amount = self.direction * time.dt * self.speed
         self.position += move_amount
 
-        
+        if held_keys['right mouse']:
+            camera.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
+
+        if not held_keys['right mouse'] and self.direction != self.forward:
+            camera.rotation = lerp(camera.rotation, self.forward, 1 * time.dt)
 
     def input(self, key):
         ...
+
+
+
+if __name__ == '__main__':
+    app = Ursina()
+    ThirdPersonController()
+    Entity(model='cube', x=3, z=2)
+    Sky()
+    app.run()

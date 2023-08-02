be44f94828d12a40c1c971ee5f513184e1d36404
developer-1v <1v.all.games@gmail.com>
2023-08-02 13:25:17 -0700

    - update for animations

--- src/core1k/player_controllers/third_person_controller.py
+++ src/core1k/player_controllers/third_person_controller.py
@@ -1,13 +1,19 @@
 from ursina import *
+from direct.interval.ActorInterval import LerpAnimInterval
+from direct.actor.Actor import Actor
+from print_tricks import pt
 
 class ThirdPersonController(Entity):
-    def __init__(self, height=2, speed=2, **kwargs):
+    def __init__(self, height=1, speed=2, **kwargs):
         super().__init__(**kwargs)
+        
         self.height = height
+        self.y = 0
         self.speed = speed
+        self.rotation_speed = 88
 
-        self.model = "cube"
-        self.color = color.red
+        # self.model = "cube"
+        # self.color = color.red
 
         self.mouse_sensitivity = Vec2(40, 40)
 
@@ -17,8 +23,15 @@ class ThirdPersonController(Entity):
         camera.z = -3.25
 
         mouse.locked = True
-
+        
+        self.actor = Actor()
+        
+        self.direction = (1,1,1) ## setting this with initial starting point so code in update can work right. 
+        self.last_direction = (0,0,0)
+        
     def update(self):
+        # self.last_direction = self.direction
+        
         direction = Vec3(
             self.forward * ((held_keys['w'] - held_keys['s']) or held_keys['gamepad left stick y'])
             + self.right * ((held_keys['e'] - held_keys['q']) or held_keys['gamepad right stick x'])
@@ -29,17 +42,32 @@ class ThirdPersonController(Entity):
         self.position += move_amount
 
         rotation = ((held_keys['d'] - held_keys['a']))
-        self.rotation_y += rotation
-
+        self.rotation_y += rotation * time.dt * self.rotation_speed
+        
         if held_keys['right mouse']:
             camera.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
 
         if not held_keys['right mouse'] and self.direction != self.forward:
             camera.rotation = lerp(camera.rotation, self.forward, 1 * time.dt)
 
-    def input(self, key):
-        ...
 
+
+    def blend_anim(actor, animation, duration = .75, loop = True):
+        actor.enableBlend()
+        if loop:
+            actor.loop(animation)
+        else:
+            actor.play(animation)
+        blend = LerpAnimInterval(actor, duration, actor.getCurrentAnim(), animation)
+        blend.start()
+        
+    def play_animation(self, action):
+        if action in self.anims:
+            self.actor.stop()  # Stop the current animation
+            self.actor.loop(action)
+#         else:
+#             self.actor.stop()
+#             pt('stop')
 
 
 if __name__ == '__main__':
@@ -47,4 +75,5 @@ if __name__ == '__main__':
     ThirdPersonController()
     Entity(model='cube', x=3, z=2)
     Sky()
+
     app.run()

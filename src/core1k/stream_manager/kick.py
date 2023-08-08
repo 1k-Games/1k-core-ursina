ba14de93244fc7137bfcda148ea4e918898457fc
konvay <parisschuette@gmail.com>
2023-08-07 19:45:09 -0500

    undetectable chrome

--- src/core1k/stream_manager/kick.py
+++ src/core1k/stream_manager/kick.py
@@ -1,7 +1,6 @@
-from requests_html import HTMLSession
+import undetected_chromedriver as uc
 
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 import json
@@ -11,15 +10,15 @@ class HeadlessChrome():
         url = "https://kick.com/"
         chrome_options = Options()
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
 
-        self.driver = webdriver.Chrome(chrome_options)
+        self.driver = uc.Chrome(chrome_options, use_subprocess=True)
         self.driver.get(url)
 
 class Kick():
     def __init__(self, channel_name):
         self.channel_name = channel_name
 
-        self.session = HTMLSession()
         self.browser = HeadlessChrome()
 
         self.channel_data = self.channel()

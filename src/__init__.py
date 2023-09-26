6df8328e97a7667c745c847b8ff8b33b4219c9e1
1v_All <developer_1v@proton.me>
2023-09-26 15:12:02 -0700

    changed init location for global * imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 ﻿from controllers.controls_center import ControlsCenter 
 from controllers.first_person_shooter_controller import FirstPersonShooterController
+from controllers.third_person_controller import ThirdPersonController
 from controllers.free_camera import FreeCamera
 from controllers.orbital_camera import OrbitalCamera
-from controllers.third_person_controller import ThirdPersonController
 
 from dev_tools.dev_pause_menu import DevPauseMenu
 from dev_tools.menu_1k import MenuTemplate

b9b1ab74b4f9139ce9b2fdc2797392ae9c92ee23
1v_All <developer_1v@proton.me>
2024-02-04 02:52:45 -0800

    further testing completed for passing vars between a prepare_function and a use_ function

--- src/core1k/Allbilities/new_ccaus_combility_tests/mod_category_one.py
+++ src/core1k/Allbilities/new_ccaus_combility_tests/mod_category_one.py
@@ -7,9 +7,16 @@ class Mod_One_A:
         
     def prepare_mod_one_a(self, a, b='mod_one_a default kwarg'):
         pt(a)
+        setattr(self, 'a', a)
+        setattr(self, 'b', b)
     
     def use_mod_one_a(self, b='optional kwarg'):
         pt(b)
+        a = getattr(self, 'a')
+        b = getattr(self, 'b')
+        pt('We have found the vars, a & b: ')
+        pt(a, b)
+        pt(fr'thats it')
         
 class Mod_One_B:
     def use_mod_one_b(self, b, c='mod_one_b default kwarg'):
